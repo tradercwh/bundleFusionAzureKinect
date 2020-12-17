fdbb6367e2ea96f90a8a9cc9362a936c19d995d1
Henry Chen <646866479@qq.com>
2020-12-17 11:20:35 +0800

    new theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,14 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
 # -- Project information -----------------------------------------------------
 
-project = 'bundlefusion_azureKinect'
+project = 'Bundle Fusion With Azure Kinect'
 copyright = '2020, tau'
 author = 'tau'
 
@@ -47,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
